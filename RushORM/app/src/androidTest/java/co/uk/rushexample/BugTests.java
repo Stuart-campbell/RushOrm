e1cc672b802c3d01af908499345a7fc17b6e8e93
Stuart Campbell <stuartc@apadmi.com>
2015-02-19 16:36:13 +0000

    Test added for SQL Error when updating objects #7 fix to come shortly

--- RushORM/app/src/androidTest/java/co/uk/rushexample/BugTests.java
+++ RushORM/app/src/androidTest/java/co/uk/rushexample/BugTests.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 import co.uk.rushexample.testobjects.Bug6;
+import co.uk.rushexample.testobjects.Bug7Child;
+import co.uk.rushexample.testobjects.Bug7Parent;
 import co.uk.rushexample.testobjects.TestObject;
 import co.uk.rushorm.android.RushAndroid;
 import co.uk.rushorm.core.RushSearch;
@@ -41,5 +43,25 @@ public class BugTests extends ApplicationTestCase<Application> {
         assertNotNull(loadedUser);
     }
 
+    public void testBug7() throws Exception {
 
+        Bug7Parent parent = new Bug7Parent();
+        parent.save();
+
+        Bug7Child firstChild = new Bug7Child("String");
+        firstChild.save();
+
+        Bug7Child secondChild = new Bug7Child("String");
+        secondChild.save();
+
+        parent.add(firstChild);
+        parent.add(secondChild);
+        parent.save();
+
+        parent = new RushSearch().find(Bug7Parent.class).get(0);
+        parent.getChildren().get(0).setString("test1");
+        parent.save();
+
+        assertNotNull(parent);
+    }
 }
